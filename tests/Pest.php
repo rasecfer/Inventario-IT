e0bceb37b16a970cb3d197897d551f086fe862d8
Ing. Fernando Moreno <rasecfer@MacBook-Air-de-Ing.local>
2026-06-02 23:35:09 -0600

    Remove Fortify Tests from Pest.php

--- tests/Pest.php
+++ tests/Pest.php
@@ -14,9 +14,9 @@ use Tests\TestCase;
 |
 */
 
-pest()->extend(TestCase::class)
-    ->use(RefreshDatabase::class)
-    ->in('Feature');
+// pest()->extend(TestCase::class)
+//     ->use(RefreshDatabase::class)
+//     ->in('Feature');
 
 /*
 |--------------------------------------------------------------------------
