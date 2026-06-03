4066ded7b42d80c346d6417ce351e95110bbbbd7
Ing. Fernando Moreno <rasecfer@MacBook-Air-de-Ing.local>
2026-06-02 23:40:59 -0600

    Remove Fortify Tests added Feature folder

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,10 +7,10 @@ use Laravel\Fortify\Features;
 
 abstract class TestCase extends BaseTestCase
 {
-    protected function skipUnlessFortifyHas(string $feature, ?string $message = null): void
-    {
-        if (! Features::enabled($feature)) {
-            $this->markTestSkipped($message ?? "Fortify feature [{$feature}] is not enabled.");
-        }
-    }
+    // protected function skipUnlessFortifyHas(string $feature, ?string $message = null): void
+    // {
+    //     if (! Features::enabled($feature)) {
+    //         $this->markTestSkipped($message ?? "Fortify feature [{$feature}] is not enabled.");
+    //     }
+    // }
 }
