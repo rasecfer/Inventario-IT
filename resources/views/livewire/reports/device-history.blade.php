9e00c9bbb73c0daddb0a0891b5083ca202768a71
Fernando Moreno <rasecfer@gmail.com>
2026-07-07 10:37:40 -0600

    Added icon to History

--- resources/views/livewire/reports/device-history.blade.php
+++ resources/views/livewire/reports/device-history.blade.php
@@ -77,7 +77,14 @@
                                 <div class="font-bold text-slate-900 dark:text-slate-100">{{ $item['type'] }} - Folio:
                                     {{ $item['id'] }}</div>
                                 <time class="font-caveat text-sm font-medium text-emerald-500">
-                                    {{ \Carbon\Carbon::parse($item['date'])->format('d/m/Y') }}
+                                    <div class="flex items-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor" class="mr-1 size-4">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+                                        </svg>
+                                        {{ \Carbon\Carbon::parse($item['date'])->format('d/m/Y') }}
+                                    </div>
                                 </time>
                             </div>
                             <div class="text-sm text-slate-500 dark:text-slate-400">{{ $item['comments'] }}</div>
